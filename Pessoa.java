3c220cd24dabeae3178ccd1756e886badc4f0f3b
Marcelo Gabriel <marcelogabrielcn@outlook.com>
2021-02-26 09:33:06 -0300

    Segundo Commit

--- Pessoa.java
+++ Pessoa.java
@@ -1,9 +1,17 @@
 
 public class Pessoa {
 
+	String nome = "";
+	double peso = 0;
+	double altura = 0;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Pessoa p1 = new Pessoa();  //Instanciando valor ao atributo
+		p1.nome = "Marcelo Gabriel";
+		p1.peso = 80.20;
+		p1.altura = 1.86;
+		
 	}
 
 }
