8ddf0fba7d6876e52a76df5930b3af64e6602f19
Marcelo Gabriel <marcelogabrielcn@outlook.com>
2021-02-26 10:42:58 -0300

    Primeiro Commit
    Aprendendo o conceito de Condicionais, aqui usei a condicional IF

---
+++ Condicional.java
@@ -0,0 +1,18 @@
+
+public class Condicional {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 150;
+		if (a < 10) {
+		System.out.println("o Valor de a é menor que dez");
+		} else if (a < 100) {
+		System.out.println("o Valor de a é menor que cem");
+		} else if (a < 1000) {
+		System.out.println("o Valor de a é menor que mil");
+		} else {
+		System.out.println("o Valor de a é muito grande");
+		}
+	}
+
+}
