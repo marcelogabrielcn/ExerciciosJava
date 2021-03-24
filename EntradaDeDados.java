3b55518749fc295f738cfe5384ddddad784391b9
Marcelo Gabriel <marcelogabrielcn@outlook.com>
2021-03-24 16:56:29 -0300

    Primeiro Commit

--- EntradaDeDados.java
+++ EntradaDeDados.java
@@ -1,9 +1,15 @@
-import javax.swing.*;
+package sala06;
+
+import java.util.Scanner;
 
 public class EntradaDeDados {
     public static void main(String[] args) {
-        String texto = JOptionPane.showInputDialog(null, "Digite algum texto: ",
-                "Teste - Entrada de Dados", JOptionPane.PLAIN_MESSAGE);
-    System.out.println("Sua mensagem foi: " + texto);
+        //Para utilizar entrada de dados pelo teclado temos que importar uma biblioteca
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Digite seu nome: ");
+        String nome = teclado.nextLine();
+        System.out.print("Digite sua idade: ");
+        int idade = teclado.nextInt();
+        System.out.printf("Olá %s, você tem %d anos \n", nome, idade);
     }
 }
