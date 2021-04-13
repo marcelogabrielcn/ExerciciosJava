be9ca06df61c7c08e9357bc3f0a8abc5d3f5b053
Marcelo Gabriel <marcelogabrielcn@outlook.com>
2021-04-13 06:35:55 -0300

    Primeiro Commit
    Aprendendo usar operadores lógicos

---
+++ sala08/OperadorLogico.java
@@ -0,0 +1,14 @@
+package sala08;
+
+public class OperadorLogico {
+    public static void main(String[] args) {
+        int x, y, z;
+        x = 5;
+        y = 8;
+        z = 13;
+        boolean r;
+        r = (x>y && y<z)?true:false;
+        System.out.println(r);
+
+    }
+}
